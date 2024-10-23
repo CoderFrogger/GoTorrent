11e0e870023baed7f8262970e707eabd2a62c9d2
SCP-079 <scp-079@archlinux.localdomain>
2024-10-23 16:40:26 +0300

    Create structures and initiate decode.go

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 	"os"
 )
 
+type Torrent struct {
+	Announce string `bencode:"announce"`
+	Info     Info   `bencode:"info"`
+}
+
+type Info struct {
+	Name        string `bencode:"name"`
+	Length      int    `bencode:"length"`
+	PieceLength int    `bencode:"piece length"`
+	Pieces      string `bencode:"pieces"`
+}
+
 func main() {
 	switch command := os.Args[1]; command {
 	case "decode":
