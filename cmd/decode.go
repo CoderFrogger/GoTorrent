3c44c5baf46b03d04334c3ed94ede517ad7ec2c7
SCP-079 <scp-079@archlinux.localdomain>
2024-10-23 17:08:39 +0300

    decode bencoded integer

--- cmd/decode.go
+++ cmd/decode.go
@@ -1,15 +1,48 @@
 package cmd
 
-import "unicode"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
-func decodeBencode(benString string, startPos int) {
+func decodeBencode(benString string, startPos int) (interface{}, int, error) {
 	switch {
 	case benString[startPos] == 'i':
+		return decodeBenInt(benString, startPos)
 	case benString[startPos] == 'l':
+		return nil, 0, nil
 	case unicode.IsDigit(rune(benString[startPos])):
+		return nil, 0, nil
 	case benString[startPos] == 'd':
+		return nil, 0, nil
 	default:
-		return
+		return nil, 0, nil
 
 	}
+}
+
+func decodeBenInt(benString string, startPos int) (interface{}, int, error) {
+	benIntEnd := strings.Index(benString[startPos:], "e") + startPos
+	var decodedInt int
+	var err error
+	nextElementIndex := benIntEnd + 1
+
+	if benString[1] == '-' {
+		decodedInt, err = strconv.Atoi(benString[startPos+2 : benIntEnd])
+		if err != nil {
+			fmt.Println("Error during int decode: ", err)
+			return nil, 0, err
+		}
+		decodedInt = -(decodedInt)
+	} else {
+		decodedInt, err = strconv.Atoi(benString[startPos+1 : benIntEnd])
+		if err != nil {
+			fmt.Println("Error during int decode: ", err)
+			return nil, 0, err
+		}
+	}
+
+	return decodedInt, nextElementIndex, nil
 }
