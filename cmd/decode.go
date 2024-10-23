8e2f6196d2c6fab34354fb00e0314f78d8436da6
SCP-079 <scp-079@archlinux.localdomain>
2024-10-23 16:53:13 +0300

    Create decoding main func

--- cmd/decode.go
+++ cmd/decode.go
@@ -1,4 +1,15 @@
 package cmd
 
-func decodeBencode() {
+import "unicode"
+
+func decodeBencode(benString string, startPos int) {
+	switch {
+	case benString[startPos] == 'i':
+	case benString[startPos] == 'l':
+	case unicode.IsDigit(rune(benString[startPos])):
+	case benString[startPos] == 'd':
+	default:
+		return
+
+	}
 }
