fe9511fba8e50cbe2b916867471de6594312dd1f
SCP-079 <scp-079@archlinux.localdomain>
2024-11-12 08:06:57 +0200

    Add error logging

--- cmd/peers.go
+++ cmd/peers.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -24,18 +25,27 @@ func DiscoverPeers(torrent Torrent) (TrackerResponse, error) {
 
 	response, err := http.Get(torrent.Announce + "?" + query.Encode())
 	if err != nil {
-		return TrackerResponse{}, err
+		return TrackerResponse{}, fmt.Errorf(
+			"Error getting response from peer: %s\n",
+			err,
+		)
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return TrackerResponse{}, err
+		return TrackerResponse{}, fmt.Errorf(
+			"Error reading response: %s\n",
+			err,
+		)
 	}
 
 	buffer, _, err := DecodeBenDictionary(string(body), 0)
 	if err != nil {
-		return TrackerResponse{}, err
+		return TrackerResponse{}, fmt.Errorf(
+			"Error decoding dictionary: %s\n",
+			err,
+		)
 	}
 
 	var tracker TrackerResponse
